e2762cfab5733d686fd992839b79f078d9451468
Anant Shrivastava <anant@anantshri.info>
2014-11-30 12:40:33 +0530

    Initial Commit
    Basic ironwasp installer in place

---
+++ ironwasp_installer.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+echo "======================================================================================= "
+echo "|  _____  _______      ___   ____  _____  ____      ____  _       ______   _______    | "
+echo "| |_   _||_   __ \   .'   \`.|_   \|_   _||_  _|    |_  _|/ \    .' ____ \ |_   __ \  | "
+echo "|   | |    | |__) | /  .-.  \ |   \ | |    \ \  /\  / / / _ \   | (___ \_|  | |__) |  | "
+echo "|   | |    |  __ /  | |   | | | |\ \| |     \ \/  \/ / / ___ \   _.____\`.   |  ___/  | "
+echo "|  _| |_  _| |  \ \_\  \`-'  /_| |_\   |_     \  /\  /_/ /   \ \_| \____) | _| |_     | "
+echo "| |_____||____| |___|\`.___.'|_____|\____|     \/  \/|____| |____|\______.'|_____|    | "
+echo "|                                                                                     | "
+echo "======================================================================================= "
+echo "                          IRONWASP Linux / MAC Installer                                "
+echo ""
+echo "Will automatically install and configure IRONWASP."
+echo "All you need to click is next next next"
+echo ""
+echo "Script Written by Anant shrivastava http://anantshri.info"
+echo "Grab the latest script here : https://github.com/anantshri/ironwasp_installer"
+echo ""
+read -p "press any key to continue" input_cmd
+# check if wine is installed.
+if which wine >/dev/null; then
+	echo "Creating directories"
+	export WINEARCH=win32
+	PTH="${HOME}/IRONWASP"
+	iPTH=$PTH"/installer"
+	#mkdir $PTH
+	echo "Starting Wine configuration"
+	WINEPREFIX=$PTH wineboot
+	mkdir $iPTH
+	wget http://winetricks.org/winetricks -O $iPTH/winetricks
+	chmod 755 $iPTH/winetricks
+	WINEPREFIX=$PTH sh $iPTH/winetricks dotnet20sp2 fontfix
+	wget http://ironwasp.org/ironwasp.zip -O $iPTH/ironwasp.zip
+	unzip $iPTH/ironwasp.zip -d $PTH/drive_c/
+	#rm -rf $PTH
+	#create a script on desktop
+	#cat << EOF  > ~/Desktop/IronWasp
+	#!/bin/bash
+	#cd $PTH
+	#WINEPREFIX=$PTH wine 'c:/IronWASP/IronWASP.exe'
+	#EOF
+	#chmod +x ~/Desktop/IronWasp
+	if [ "$(uname)" == "Darwin" ]; then
+	    # Do something under Mac OS X platform        
+	    echo "Mac detected"
+	    echo "Creating a mac application"
+	    if [ -a "~/Applications/IronWASP.app" ]
+	    	echo "IronWASP Application entry already exist"
+	    	echo "Rerun the whole setup if you thing this is wrong"
+	    	exit
+	    else
+		    mkdir -p ~/Applications/IronWASP.app/Contents/MacOS
+		    cat << EOF  > $PTH/IronWASP.app/Contents/MacOS/IronWASP
+			#!/bin/bash
+			cd $PTH
+			WINEPREFIX=$PTH wine 'c:/IronWASP/IronWASP.exe'
+			EOF
+			chmod +x $PTH/IronWASP.app/Contents/MacOS/IronWASP
+		fi
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		echo "Linux detected"
+		echo "Shortcut created on desktop"
+		cat << EOF  > ~/Desktop/IronWasp
+		#!/bin/bash
+		cd $PTH
+		WINEPREFIX=$PTH wine 'c:/IronWASP/IronWASP.exe'
+		EOF
+		chmod +x ~/Desktop/IronWasp
+	fi
+else
+    echo "We need Wine for this process to work properly"
+    echo "Please install wine as per your system"
+    exit "you may want to check apt-get , yum, brew, macport before downloading "
+fi
